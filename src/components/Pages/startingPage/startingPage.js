a2701aa9287f10c7fed6569bde8e19ce537dd105
SlowBurn <VanhelsingX3000@gmail.com>
2022-07-05 18:47:17 +0300

    rebuild fix iframe

--- src/components/Pages/startingPage/startingPage.js
+++ src/components/Pages/startingPage/startingPage.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import './startingPage.css';
-<<<<<<< HEAD
-import startVideo from "./thewheel-got-acard_h1080p.mov";
-=======
->>>>>>> master
 
 
 const StartingPage = () => {
 
-<<<<<<< HEAD
     return (
         <iframe width="716" height="403" src="https://www.youtube.com/embed/rlR4PJn8b8I" title="Game of Thrones | Season 8 | Official Trailer (HBO)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     )
 
 }
 
-=======
-    return (
-        <iframe width="716" height="403" src="https://www.youtube.com/embed/rlR4PJn8b8I" title="Game of Thrones | Season 8 | Official Trailer (HBO)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    )
-
-}
-
->>>>>>> master
 export default StartingPage;
